c62cba097e3055343d96d33b2a48974a77b2a337
Tamim Karim <ittamim22@gmail.com>
2023-11-27 06:50:30 +0600

    middle of work

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import fakeData from '../../fakeData';
+import { useState } from 'react';
+import './Shop.css'
+import Product from '../Product/Product';
+
+
+const Shop = () => {
+    const first10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState(first10);
+
+    const handleAddProduct = (product) =>{
+        console.log('Product Added', product);
+    }
+
+
+    return (
+        <div className="shop-container">
+           
+           <div className="product-container">
+            
+            {
+                products.map(pd => <Product
+                handleAddProduct = {handleAddProduct}   
+                product ={pd}
+                ></Product>)
+            }
+            
+           </div>
+            <div className="cart-container">
+                <h3>This is cart</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
